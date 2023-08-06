6c5a5ed2af42d30ba6802e38777d1dfb0f77f011
Italo <italofsilva583@gmail.com>
2023-08-06 17:08:43 -0300

    Atualização nas tecnologias

--- src/componentes/Tecnologias/Tecnologias.jsx
+++ src/componentes/Tecnologias/Tecnologias.jsx
@@ -15,35 +15,35 @@ import iconLinux from './img/linux.png'
 export default function Tecnologias(props) {
     
     function descHTML() {
-        document.querySelector('#descricao').innerHTML = 'HTML'
+        document.querySelector('#descricao').innerHTML = 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores.'
     }
 
     function descCSS() {
-        document.querySelector('#descricao').innerHTML = 'CSS'
+        document.querySelector('#descricao').innerHTML = 'CSS (Cascading Style Sheets) é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML ou ficar contido dentro das tags style.'
     }
 
     function descJS() {
-        document.querySelector('#descricao').innerHTML = 'JavaScript'
+        document.querySelector('#descricao').innerHTML = 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.'
     }
 
     function descREACT() {
-        document.querySelector('#descricao').innerHTML = 'React'
+        document.querySelector('#descricao').innerHTML = 'O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.'
     }
 
     function descBootStrap() {
-        document.querySelector('#descricao').innerHTML = 'BootStrap'
+        document.querySelector('#descricao').innerHTML = 'Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web, usando HTML, CSS e JavaScript, melhorando a experiência do usuário em um site amigável e responsivo.'
     }
 
     function descPython() {
-        document.querySelector('#descricao').innerHTML = 'Python'
+        document.querySelector('#descricao').innerHTML = 'Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.'
     }
 
     function descGit() {
-        document.querySelector('#descricao').innerHTML = 'Git'
+        document.querySelector('#descricao').innerHTML = 'Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo de arquivo.'
     }
 
     function descLinux() {
-        document.querySelector('#descricao').innerHTML = 'Linux'
+        document.querySelector('#descricao').innerHTML = 'Linux é um termo popularmente empregado para se referir a sistemas operativos ou sistemas operacionais que utilizam o núcleo Linux. O núcleo foi desenvolvido pelo programador finlandês Linus Torvalds, inspirado no sistema Minix.'
     }
 
     function retornarDesc() {
@@ -82,7 +82,7 @@ export default function Tecnologias(props) {
                 
                 <div class="col-md-4 DivDescricao">
                     
-                    <h1 id='descricao'></h1>
+                    <h1 id='descricao' className='Descricao'></h1>
                 </div>
 
                 
