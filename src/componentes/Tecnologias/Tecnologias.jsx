13bbb54e3a0304a5e714016ba0f0d0fd03cbe505
Italo <italofsilva583@gmail.com>
2023-08-06 12:59:19 -0300

    Atualização nas tecnologias

--- src/componentes/Tecnologias/Tecnologias.jsx
+++ src/componentes/Tecnologias/Tecnologias.jsx
@@ -14,9 +14,41 @@ import iconLinux from './img/linux.png'
 
 export default function Tecnologias(props) {
     
+    function descHTML() {
+        document.querySelector('#descricao').innerHTML = 'HTML'
+    }
 
+    function descCSS() {
+        document.querySelector('#descricao').innerHTML = 'CSS'
+    }
 
+    function descJS() {
+        document.querySelector('#descricao').innerHTML = 'JavaScript'
+    }
 
+    function descREACT() {
+        document.querySelector('#descricao').innerHTML = 'React'
+    }
+
+    function descBootStrap() {
+        document.querySelector('#descricao').innerHTML = 'BootStrap'
+    }
+
+    function descPython() {
+        document.querySelector('#descricao').innerHTML = 'Python'
+    }
+
+    function descGit() {
+        document.querySelector('#descricao').innerHTML = 'Git'
+    }
+
+    function descLinux() {
+        document.querySelector('#descricao').innerHTML = 'Linux'
+    }
+
+    function retornarDesc() {
+        document.querySelector('#descricao').innerHTML = ''
+    }
 
     return(
         <section className='container text-center DivTec' id='conhecimentos'>
@@ -31,16 +63,17 @@ export default function Tecnologias(props) {
         
                 />
                 <div class="col-md-4 DivTecnologias">
+                        {/* <button onClick={descHTML} >teste</button> */}
                     {/* <div> */}
-                      <img src={iconHTML} id='iconHTML' className='Icon'alt="" />
-                      <img src={iconCSS} id='iconCSS' className='Icon'alt="" />
-                      <img src={iconJS} id='iconJS' className='Icon'alt="" />
-                      <img src={iconREACT} id='iconREACT' className='Icon'alt="" />
+                      <img src={iconHTML} onMouseEnter={descHTML} onMouseLeave={retornarDesc} id='iconHTML' className='Icon'alt="" />
+                      <img src={iconCSS} onMouseEnter={descCSS} onMouseLeave={retornarDesc} id='iconCSS' className='Icon'alt="" />
+                      <img src={iconJS} onMouseEnter={descJS} onMouseLeave={retornarDesc} id='iconJS' className='Icon'alt="" />
+                      <img src={iconREACT} onMouseEnter={descREACT} onMouseLeave={retornarDesc} id='iconREACT' className='Icon'alt="" />
                       
-                      <img src={iconBootStrap} id='iconBootStrap' className='Icon'alt="" />
-                      <img src={iconPython} id='iconPython' className='Icon'alt="" />
-                      <img src={iconGit} id='iconGit' className='Icon'alt="" />
-                      <img src={iconLinux} id='iconLinux' className='Icon'alt="" />
+                      <img src={iconBootStrap} onMouseEnter={descBootStrap} onMouseLeave={retornarDesc} id='iconBootStrap' className='Icon'alt="" />
+                      <img src={iconPython} onMouseEnter={descPython} onMouseLeave={retornarDesc} id='iconPython' className='Icon'alt="" />
+                      <img src={iconGit} onMouseEnter={descGit} onMouseLeave={retornarDesc}id='iconGit' className='Icon'alt="" />
+                      <img src={iconLinux} onMouseEnter={descLinux} onMouseLeave={retornarDesc} id='iconLinux' className='Icon'alt="" />
 
                     {/* </div> */}
 
